fd1b896f4500ee8016d586b6d9e80159bd1d725d
var512 <var512dev@gmail.com>
2020-08-03 00:32:12 -0300

    Update usage message for all flags

--- cmd/deskgen/deskgen.go
+++ cmd/deskgen/deskgen.go
@@ -11,34 +11,35 @@ import (
 
 var (
 	// Command.
-	filePath = flag.String("filePath", "", "FilePath")
-	fileName = flag.String("fileName", "", "FileName")
+	filePath = flag.String("filePath", "", "filesystem path to write the file to")
+	fileName = flag.String("fileName", "", "file name without extension")
 
-	// Desktop Entry Specification.
-	typeKey              = flag.String("type", "", "Type")
-	name                 = flag.String("name", "", "Name")
-	version              = flag.String("version", "", "Version")
-	genericName          = flag.String("genericName", "", "GenericName")
-	noDisplay            = flag.Bool("noDisplay", false, "NoDisplay")
-	comment              = flag.String("comment", "", "Comment")
-	icon                 = flag.String("icon", "", "Icon")
-	hidden               = flag.Bool("hidden", false, "Hidden")
-	onlyShowIn           = flag.String("onlyShowIn", "", "OnlyShowIn")
-	notShowIn            = flag.String("notShowIn", "", "NotShowIn")
-	dbusActivatable      = flag.Bool("dbusActivatable", false, "DBusActivatable")
-	tryExec              = flag.String("tryExec", "", "TryExec")
-	exec                 = flag.String("exec", "", "Exec")
-	path                 = flag.String("path", "", "Path")
-	terminal             = flag.Bool("terminal", false, "Terminal")
-	mimeType             = flag.String("mimeType", "", "MimeType")
-	categories           = flag.String("categories", "", "Categories")
-	implements           = flag.String("implements", "", "Implements")
-	keywords             = flag.String("keywords", "", "Keywords")
-	startupNotify        = flag.Bool("startupNotify", false, "StartupNotify")
-	startupWMClass       = flag.String("startupWMClass", "", "StartupWMClass")
-	URL                  = flag.String("url", "", "URL")
-	prefersNonDefaultGPU = flag.Bool("prefersNonDefaultGPU", false, "PrefersNonDefaultGPU")
+	// Desktop Entry.
+	typeKey              = flag.String("type", "", "entry key: Type")
+	name                 = flag.String("name", "", "entry key: Name")
+	version              = flag.String("version", "", "entry key: Version")
+	genericName          = flag.String("genericName", "", "entry key: GenericName")
+	noDisplay            = flag.Bool("noDisplay", false, "entry key: NoDisplay")
+	comment              = flag.String("comment", "", "entry key: Comment")
+	icon                 = flag.String("icon", "", "entry key: Icon")
+	hidden               = flag.Bool("hidden", false, "entry key: Hidden")
+	onlyShowIn           = flag.String("onlyShowIn", "", "entry key: OnlyShowIn")
+	notShowIn            = flag.String("notShowIn", "", "entry key: NotShowIn")
+	dbusActivatable      = flag.Bool("dbusActivatable", false, "entry key: DBusActivatable")
+	tryExec              = flag.String("tryExec", "", "entry key: TryExec")
+	exec                 = flag.String("exec", "", "entry key: Exec")
+	path                 = flag.String("path", "", "entry key: Path")
+	terminal             = flag.Bool("terminal", false, "entry key: Terminal")
+	mimeType             = flag.String("mimeType", "", "entry key: MimeType")
+	categories           = flag.String("categories", "", "entry key: Categories")
+	implements           = flag.String("implements", "", "entry key: Implements")
+	keywords             = flag.String("keywords", "", "entry key: Keywords")
+	startupNotify        = flag.Bool("startupNotify", false, "entry key: StartupNotify")
+	startupWMClass       = flag.String("startupWMClass", "", "entry key: StartupWMClass")
+	URL                  = flag.String("url", "", "entry key: URL")
+	prefersNonDefaultGPU = flag.Bool("prefersNonDefaultGPU", false, "entry key: PrefersNonDefaultGPU")
 
+	// Desktop Action.
 	actionName flags.ActionName
 	actionIcon flags.ActionIcon
 	actionExec flags.ActionExec
@@ -88,9 +89,9 @@ func createFile(path, name string, entry desktop.Entry) *desktop.File {
 }
 
 func main() {
-	flag.Var(&actionName, "actionName", "Action.Name")
-	flag.Var(&actionIcon, "actionIcon", "Action.Icon")
-	flag.Var(&actionExec, "actionExec", "Action.Exec")
+	flag.Var(&actionName, "actionName", "action key: Name")
+	flag.Var(&actionIcon, "actionIcon", "action key: Icon")
+	flag.Var(&actionExec, "actionExec", "action key: Exec")
 
 	flag.Parse()
 
